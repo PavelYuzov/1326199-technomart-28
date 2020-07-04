231b41dce84eb9e38d0ad05ca7d70152e43f4f07
PavelYuzov <fot.py@yandex.ru>
2020-07-04 15:58:19 +0300

    JS index

--- js/popup.js
+++ js/popup.js
@@ -8,6 +8,29 @@ var comment = document.querySelector(".input-area1");
 var linkmap = document.querySelector(".contacts-section__img");
 var map = document.querySelector(".popup-map-off");
 var mapclose = document.querySelector(".map__close");
+var buttonactive = document.querySelector(".button-active");
+var button1 = document.querySelector(".services-section-item__button1");
+var button2 = document.querySelector(".services-section-item__button2");
+var button3 = document.querySelector(".services-section-item__button3");
+var delivery = document.querySelector(".delivery-wrap");
+var guarantee = document.querySelector(".guarantee-wrap");
+var credit = document.querySelector(".credit-wrap");
+var services = document.querySelectorAll(".services-section-item");
+var services1 = document.querySelectorAll(".services-section-item");
+var hidden = document.querySelector(".section-hidden");
+var perfo = document.querySelector(".promo-perforator");
+var drill = document.querySelector(".promo-drill");
+var none = document.querySelector(".promo-none");
+var next = document.querySelector(".widget-promo__slide-next");
+var previous = document.querySelector(".widget-promo__slide-previous");
+var bgperfo = document.querySelector(".widget-promo-perfo");
+var radio1 = document.querySelector(".widget-promo-radio1");
+var radio2 = document.querySelector(".widget-promo-radio2");
+var popupbasket = document.querySelector(".popup-basket");
+var popupbasketoff = document.querySelector(".popup-basket-off");
+var popupbasketclose = document.querySelector(".popup-basket__close");
+var popupbasketbutton = document.querySelector(".popup-basket-button");
+var buttonbasket = document.querySelector(".product-card-choice__link");
 
 var isStorageSupport = true;
 var storage = "";
@@ -69,16 +92,6 @@ mapclose.addEventListener("click", function(evt) {
   map.classList.add("popup-map-off");
 });
 
-
-var perfo = document.querySelector(".promo-perforator");
-var drill = document.querySelector(".promo-drill");
-var none = document.querySelector(".promo-none");
-var next = document.querySelector(".widget-promo__slide-next");
-var previous = document.querySelector(".widget-promo__slide-previous");
-var bgperfo = document.querySelector(".widget-promo-perfo");
-var radio1 = document.querySelector(".widget-promo-radio1");
-var radio2 = document.querySelector(".widget-promo-radio2");
-
 next.addEventListener("click", function(evt) {
   evt.preventDefault();
   perfo.classList.toggle("promo-perforator");
@@ -89,6 +102,9 @@ next.addEventListener("click", function(evt) {
 
   bgperfo.classList.toggle("widget-promo-perfo");
   bgperfo.classList.toggle("widget-promo-drill");
+
+  radio2.classList.toggle("widget-promo-radio2");
+  radio1.classList.toggle("widget-promo-radio1");
 });
 
 previous.addEventListener("click", function(evt) {
@@ -101,63 +117,71 @@ previous.addEventListener("click", function(evt) {
 
   bgperfo.classList.toggle("widget-promo-perfo");
   bgperfo.classList.toggle("widget-promo-drill");
-});
 
-var buttonactive = document.querySelector(".button-active");
-var button1 = document.querySelector(".services-section-item__button1");
-var button2 = document.querySelector(".services-section-item__button2");
-var button3 = document.querySelector(".services-section-item__button3");
-var delivery = document.querySelector(".delivery-wrap");
-var guarentee = document.querySelector(".guarantee-wrap");
-var credit = document.querySelector(".credit-wrap");
-var services = document.querySelectorAll(".services-section-item");
-var services1 = document.querySelectorAll(".services-section-item");
-var hidden = document.querySelector(".section-hidden");
+  radio2.classList.toggle("widget-promo-radio2");
+  radio1.classList.toggle("widget-promo-radio1");
+});
 
 services[0].addEventListener("click", function(evt) {
   evt.preventDefault();
-
-  delivery.classList.remove(".section-hidden");
-  guarentee.classList.add(".section-hidden");
-  credit.classList.add(".section-hidden");
+  delivery.classList.remove("section-hidden");
+  guarantee.classList.add("section-hidden");
+  credit.classList.add("section-hidden");
 });
 
 services[1].addEventListener("click", function(evt) {
   evt.preventDefault();
-
-  delivery.classList.add(".section-hidden");
-  guarentee.classList.remove(".section-hidden");
-  credit.classList.add(".section-hidden");
+  delivery.classList.add("section-hidden");
+  guarantee.classList.remove("section-hidden");
+  credit.classList.add("section-hidden");
 });
 
 services[2].addEventListener("click", function(evt) {
   evt.preventDefault();
-
-  delivery.classList.add(".section-hidden");
-  guarentee.classList.add(".section-hidden");
-  credit.classList.remove(".section-hidden");
+  delivery.classList.add("section-hidden");
+  guarantee.classList.add("section-hidden");
+  credit.classList.remove("section-hidden");
 });
 
-services[0].addEventListener("click", function(evt) {
+services1[0].addEventListener("click", function(evt) {
   evt.preventDefault();
-
   button1.classList.add("button-active");
   button2.classList.remove("button-active");
   button3.classList.remove("button-active");
 });
 
-services[1].addEventListener("click", function(evt) {
+services1[1].addEventListener("click", function(evt) {
   evt.preventDefault();
-
   button1.classList.remove("button-active");
   button2.classList.add("button-active");
   button3.classList.remove("button-active");
 });
 
-services[2].addEventListener("click", function(evt) {
+services1[2].addEventListener("click", function(evt) {
   evt.preventDefault();
-
   button1.classList.remove("button-active");
   button2.classList.remove("button-active");
   button3.classList.add("button-active");
+});
+
+buttonbasket.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      popupbasketoff.classList.add("popup-basket-off");
+    }
+  })
+
+  popupbasketoff.classList.remove("popup-basket-off");
+});
+
+popupbasketclose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupbasketoff.classList.add("popup-basket-off");
+});
+
+popupbasketbutton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupbasketoff.classList.add("popup-basket-off");
 });
